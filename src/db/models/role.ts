39d062862ff9aa713d8641fe2b6570a550b50730
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-22 08:55:29 +0700

    feat: added role models

---
+++ src/db/models/role.ts
@@ -0,0 +1,44 @@
+import { DataTypes, Model, Optional} from 'sequelize'
+import connection from '../../config/dbConnect'
+
+interface RoleAttributes {
+  id?: number,
+  roleName: string | null,
+  active?: boolean | null,
+  createdAt?: Date,
+  updateAt?: Date,
+}
+
+export interface RoleInput extends Optional<RoleAttributes, 'id'> {}
+export interface RoleOutput extends Required<RoleAttributes> {}
+
+class Role extends Model<RoleAttributes, RoleInput> implements RoleAttributes{
+  public id!: number
+  public roleName!: string
+  public active!: boolean 
+  public readonly createdAt!: Date
+  public readonly updateAt!: Date
+}
+
+Role.init({
+  id:{
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.BIGINT
+  },
+  roleName: {
+    allowNull: true,
+    type: DataTypes.STRING
+  },
+  active: {
+    allowNull: true,
+    type: DataTypes.BOOLEAN
+  }
+}, {
+  timestamps: true,
+  sequelize: connection,
+  underscored: false
+})
+
+export default Role
