7e450c04ea5e6b7137561a6ad83f39ea45c7a802
Pandhu Arya <pandhu.munjalindra@gmail.com>
2024-02-22 08:54:57 +0700

    feat: create function master menu controller

---
+++ src/db/models/mastermenu.ts
@@ -0,0 +1,59 @@
+import { DataTypes, Model, Optional} from 'sequelize'
+import connection from '../../config/dbConnect'
+import SubMenu from './submenu'
+
+interface MasterMenuAttributes {
+  id?: number,
+  name?: string | null,
+  icon?: string | null,
+  ordering?: number | null,
+  active?: boolean | null,
+  createdAt?: Date,
+  updateAt?: Date,
+}
+
+export interface MasterMenuInput extends Optional<MasterMenuAttributes, 'id'> {}
+export interface MasterMenuOutput extends Required<MasterMenuAttributes> {}
+
+class MasterMenu extends Model<MasterMenuAttributes, MasterMenuInput> implements MasterMenuAttributes{
+  public id!: number
+  public name!: string
+  public icon!: string
+  public ordering!: number
+  public active!: boolean 
+  public readonly createdAt!: Date
+  public readonly updateAt!: Date
+}
+
+MasterMenu.init({
+  id:{
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.BIGINT
+  },
+  name: {
+    allowNull: true,
+    type: DataTypes.STRING
+  },
+  icon: {
+    allowNull: true,
+    type: DataTypes.TEXT
+  },
+  ordering: {
+    allowNull: true,
+    type: DataTypes.INTEGER
+  },
+  active: {
+    allowNull: true,
+    type: DataTypes.BOOLEAN
+  }
+}, {
+  timestamps: true,
+  sequelize: connection,
+  underscored: false
+})
+
+MasterMenu.hasMany(SubMenu)
+
+export default MasterMenu
